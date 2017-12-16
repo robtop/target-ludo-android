5240fc6f121c1e71ff21cf8a1b7fbf504e862466
robtop <robintoppo@gmail.com>
2017-12-16 22:11:18 +0530

    initial commit

---
+++ desktop/src/com/tgt/ludo/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.tgt.ludo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tgt.ludo.LudoGame;
+
+public class DesktopLauncher {
+
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LudoGame(), config);
+	}
+}
